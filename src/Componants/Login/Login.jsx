723924f33a000266465c3ef2337e56898fb10a52
Suraj Pratap <advocatesurajpratap95@gmail.com>
2022-09-28 22:18:18 +0530

    navbar

---
+++ src/Componants/Login/Login.jsx
@@ -0,0 +1,9 @@
+ const Login=()=>
+{
+return (
+    <div>
+        <h1>Sigh In</h1>
+    </div>
+)
+}
+export default Login
---
+++ src/Componants/Navbar/Navbar.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import styles from "./navbar.module.css";
+import {useNavigate} from "react-router-dom"
+import { Link  } from '@chakra-ui/react';
+import {BiSearchAlt2} from "react-icons/bi"
+import {BsCartCheckFill} from "react-icons/bs";
+
+import Login from '../Login/Login'
+
+const Navbar = () => {
+    const navigate = useNavigate();
+    const GoToHomePage = () => {
+        navigate("/")
+    }
+  return (
+    
+         <div className={styles.navbar} style={{backgroundColor:"rgb(255,255,255)",position:"sticky", top:"0px",zIndex:"1"}}>
+            <div className={styles.imageDiv}>
+                <img style={{cursor:"pointer"}} onClick={GoToHomePage} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1XbnIfBe7Apois52u_vH51dpElhu2KgTEsA&usqp=CAU" alt="" 
+                />
+            </div>
+            <div className={styles.links1}>
+                
+                <a href='New' className={styles.mainLink}>New</a>
+                <a href='Women' className={styles.mainLink}>Women</a>
+                <Link to="/men" className={styles.mainLink}>Men</Link>
+                <a href='Kids' className={styles.mainLink}>Kids</a>
+                <a href='Cashmere' className={styles.mainLink}>Cashmere</a>
+                <a href='Home' className={styles.mainLink}>Home</a>
+                <a href='Stories' className={styles.mainLink}>Stories</a>
+                <a href='Sale' className={styles.mainLink}>Sale</a>
+            </div>
+           
+            <div className={styles.inputDiv}>
+                <button className={styles.searchIcon}><BiSearchAlt2/></button>
+                <input  className={styles.inputSection} type="text" placeholder='Search J.Crew' />
+                
+            </div>
+
+                 <div className={styles.links2}>
+                    
+                <Login/>
+
+                </div>
+                <div className={styles.links2Icons} >
+
+                    <BsCartCheckFill style={{fontSize:"18px"
+                    
+                    }}/>
+                  
+                </div>
+        </div>
+       
+    
+    
+    
+  )
+}
+
+export default Navbar
