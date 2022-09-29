a1ae72ea7d094fe5aa27eba39dbfbd3471468f78
Suraj Pratap <advocatesurajpratap95@gmail.com>
2022-09-29 23:55:47 +0530

    Home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
 import './App.css';
 import Navbar from './Componants/Navbar/Navbar';
+import {Routes,Route} from "react-router-dom";
+import Home from './Routes/Home';
+import Men from './Componants/Men/Men';
 function App() {
   return (
     <div className="App">
       
       <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/men" element={<Men/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/Routes/Home.jsx
@@ -0,0 +1,13 @@
+const Home= () => {
+    return (
+        <div>
+            <div style={{marginTop:"18px",fontSize:"14px",fontWeight:"500"}}>
+                <h2>Have a question? We can help.</h2>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+export default Home;
